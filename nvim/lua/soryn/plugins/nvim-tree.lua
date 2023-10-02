a45ffc5c3019939c981abd43c7df5b4b027fda3c
Soryn Bächli <soryn.baechli@gmail.com>
2023-10-02 14:23:19 +0200

    feat: Add treesitter for syntax + better nvim-tree icons

--- nvim/lua/soryn/plugins/nvim-tree.lua
+++ nvim/lua/soryn/plugins/nvim-tree.lua
@@ -11,20 +11,49 @@ return {
     vim.g.loaded_netrwPlugin = 1
 
     require("nvim-tree").setup({
-        hijack_cursor = true,
-
-        -- render = {
-        --     icons = {
-        --         git_placement = "signcolumn",
-        --     }
-        -- },
-
-        filters = {
-            custom = {
-                ".DS_Store",
-                "^.git$"
-            }
-        }
+      hijack_cursor = true,
+      update_focused_file = {
+        enable = true,
+        update_cwd = true,
+      },
+      renderer = {
+        root_folder_modifier = ":t",
+        icons = {
+          glyphs = {
+            default = "",
+            symlink = "",
+            folder = {
+              arrow_open = "",
+              arrow_closed = "",
+              default = "",
+              open = "",
+              empty = "",
+              empty_open = "",
+              symlink = "",
+              symlink_open = "",
+            },
+            git = {
+              unstaged = "",
+              staged = "S",
+              unmerged = "",
+              renamed = "➜",
+              untracked = "U",
+              deleted = "",
+              ignored = "◌",
+            },
+          },
+        },
+      },
+      diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        icons = {
+          hint = "",
+          info = "",
+          warning = "",
+          error = "",
+        },
+      },
     })
   end,
 }
