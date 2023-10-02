0a5271108a491aaee16863185b1bc8cbc5129500
Soryn Bächli <soryn.baechli@gmail.com>
2023-10-02 17:58:34 +0200

    feat: Add `which-key` for keymap popup

--- nvim/lua/soryn/core/keymaps.lua
+++ nvim/lua/soryn/core/keymaps.lua
@@ -2,11 +2,10 @@
 vim.g.mapleader = " "
 
 -- normal mode
-vim.keymap.set("n", "<leader>h", ':let @/=""<CR>')          -- remove all search highlights
-vim.keymap.set("n", "<C-s>", "<cmd>w<CR>")                  -- save file
-vim.keymap.set("n", "<C-h>", "<C-w>h")                      -- move to left window
-vim.keymap.set("n", "<C-j>", "<C-w>j")                      -- move to down window
-vim.keymap.set("n", "<C-k>", "<C-w>k")                      -- move to up window
-vim.keymap.set("n", "<C-l>", "<C-w>l")                      -- move to right window
-vim.keymap.set("n", "x", '"_x')                             -- don't copy char into register after deleting
-vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<CR>") -- toggle file tree
+vim.keymap.set("n", "<leader>h", ':let @/=""<CR>', { desc = "Remove highlighting" }) -- remove all search highlights
+vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", { desc = "Save current file" }) -- save file
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" }) -- move to left window
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" }) -- move to down window
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" }) -- move to up window
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" }) -- move to right window
+vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle filetree" }) -- toggle file tree
