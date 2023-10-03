dd68a2222db864b930799301c59211e1e247ed6c
Soryn Bächli <soryn.baechli@gmail.com>
2023-10-03 15:22:36 +0200

    feat: Add `bufferline` for tabs

--- nvim/lua/soryn/plugins/init.lua
+++ nvim/lua/soryn/plugins/init.lua
@@ -12,5 +12,21 @@ return {
       "numToStr/Comment.nvim",
       event = { "BufReadPre", "BufNewFile" },
       opts = {}
+    },
+    {
+        "akinsho/bufferline.nvim",
+        version = "*",
+        dependencies = "nvim-tree/nvim-web-devicons",
+        opts = {
+            options = {
+                mode = "tabs",
+                offsets = {
+                    {
+                        filetype = "NvimTree",
+                        text_align = "left", 
+                    }
+                },
+            }
+        }
     }
 }
