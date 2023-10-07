fba088fdc0f219a42960f575dec81c817a8106ea
Soryn Bächli <soryn.baechli@gmail.com>
2023-10-07 13:32:22 +0200

    feat: Testing out telescope styling

--- nvim/lua/soryn/plugins/colorscheme.lua
+++ nvim/lua/soryn/plugins/colorscheme.lua
@@ -3,10 +3,12 @@ return {
     lazy = false,                               -- loading at start
     priority = 1000,                            -- high prio
     config = function()
-        require("github-theme").setup({
+        local theme = require("github-theme")
+
+        theme.setup({
             options = {
                 hide_end_of_buffer = true,      -- hide "~" on line
-                transparent = true,             -- paint bg
+                terminal_colors = true,
                 inverse = {                     -- inverse colors
                     match_paren = false,
                     visual = true,
@@ -14,7 +16,56 @@ return {
                 },
                 styles = {                      -- set font styling based on word
                     comments = "italic",
-                    keywords = "bold"
+                    keywords = "bold",
+                    types = "italic,bold"
+                },
+            },
+            groups = {
+                all = {
+                    TelescopeMatching = {
+                        fg = "fg1",
+                        bg = "palette.red"
+                    },
+                    TelescopeSelection = {
+                        fg = "fg1",
+                        bg = "bg1",
+                        bold = true
+                    },
+                    TelescopePromptPrefix = {
+                        bg = "bg0"
+                    },
+                    TelescopePromptNormal = {
+                        bg = "bg0",
+                    },
+                    TelescopeResultsNormal = {
+                        bg = "bg1"
+                    },
+                    TelescopePreviewNormal = {
+                        bg = "bg1"
+                    },
+                    TelescopePromptBorder = {
+                        bg = "bg0",
+                        fg = "bg0"
+                    },
+                    TelescopeResultsBorder = {
+                        bg = "bg1",
+                        fg = "bg1"
+                    },
+                    TelescopePreviewBorder = {
+                        bg = "bg1",
+                        fg = "bg1"
+                    },
+                    TelescopePromptTitle = {
+                        bg = "palette.magenta",
+                        fg = "bg1"
+                    },
+                    TelescopeResultsTitle = {
+                        fg = "fg1",
+                    },
+                    TelescopePreviewTitle = {
+                        bg = "palette.blue",
+                        fg = "bg1"
+                    }
                 }
             }
         })
--- nvim/lua/soryn/plugins/telescope.lua
+++ nvim/lua/soryn/plugins/telescope.lua
@@ -1,8 +1,18 @@
 return {
-   "nvim-telescope/telescope.nvim",
-   tag = "0.1.3",
-   dependencies = {
-       "nvim-lua/plenary.nvim",
-   },
-   opts = {}
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.3",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    config = function()
+        options = {
+            defaults = {
+                set_env = { ["COLORTERM"] = "truecolor" },
+                color_devicons = true,
+                border = {},
+                path_display = { "truncate" },
+                winblend = 0,
+            }
+        }
+    end
 }
