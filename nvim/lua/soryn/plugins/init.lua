40feb820b553a3ca59d1429abf872f0761efdc75
Soryn Bächli <soryn.baechli@gmail.com>
2023-10-10 22:01:47 +0200

    feat: Add `colorizer` + restructuring + start of `none-ls`

--- nvim/lua/soryn/plugins/init.lua
+++ nvim/lua/soryn/plugins/init.lua
@@ -1,20 +1,62 @@
 return {
     {
-      "folke/which-key.nvim", -- show popup for shortcut help
-      event = "VeryLazy",
-      init = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 300
-      end,
-      opts = {}
+        "folke/which-key.nvim", -- show popup for shortcut help
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {}
     },
     {
-      "numToStr/Comment.nvim", -- comment out lines/blocks easier
-      event = { "BufReadPre", "BufNewFile" },
-      opts = {}
+        "numToStr/Comment.nvim", -- comment out lines/blocks easier
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {}
     },
     {
         "stevearc/dressing.nvim", -- make input use telescope for better ui
         event = "VeryLazy"
+    },
+    {
+        "kylechui/nvim-surround", -- surround things a looooot easier
+        version = "*",
+        event = "VeryLazy",
+        opts = {}
+    },
+    {
+        "NvChad/nvim-colorizer.lua", -- color the background of colors
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("colorizer").setup({
+                user_default_options = {
+                    tailwind = true
+                }
+            })
+        end
+    },
+    {
+       "windwp/nvim-autopairs", -- automatically add pairs for (){}[]''""``
+        event = "InsertEnter",
+        config = function()
+            require("nvim-autopairs").setup({
+                check_ts = true
+            })
+        end
+    },
+    {
+        "lewis6991/gitsigns.nvim", -- show git line status in gutter
+        lazy = false,
+        config = function()
+            require("gitsigns").setup({
+            signs = {
+                add          = { text = '▍' },
+                change       = { text = '▍' },
+                delete       = { text = '▶' },
+                topdelete    = { text = '▶' },
+                changedelete = { text = '~' },
+                untracked    = { text = '▍' },
+              },
+            })
+        end
     }
 }
--- nvim/lua/soryn/plugins/treesitter.lua
+++ nvim/lua/soryn/plugins/treesitter.lua
@@ -1,17 +1,19 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {"lua", "typescript", "javascript", "html", "json", "markdown"}, -- make sure the following parser are installed
-            sync_install = false,
-            auto_install = true,
-            indent = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false
-            }
-        })
-    end
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {"lua", "typescript", "javascript", "html", "json", "markdown"}, -- make sure the following parser are installed
+                sync_install = false,
+                auto_install = true,
+                indent = true,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false
+                }
+            })
+        end
+    }
 }
