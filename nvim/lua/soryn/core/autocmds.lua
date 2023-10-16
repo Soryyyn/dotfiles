2f27af6a6a2f959935564ea78c7282929c8b0805
Soryn Bächli <soryn.baechli@gmail.com>
2023-10-16 12:37:47 +0200

    feat: Removed nvim-tree autocmd + added highlight yank autocmd

--- nvim/lua/soryn/core/autocmds.lua
+++ nvim/lua/soryn/core/autocmds.lua
@@ -1,23 +1,9 @@
--- close nvim-tree if it's the last buffer open
-vim.api.nvim_create_autocmd("QuitPre", {
-  callback = function()
-    local tree_wins = {}
-    local floating_wins = {}
-    local wins = vim.api.nvim_list_wins()
-    for _, w in ipairs(wins) do
-      local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
-      if bufname:match("NvimTree_") ~= nil then
-        table.insert(tree_wins, w)
-      end
-      if vim.api.nvim_win_get_config(w).relative ~= '' then
-        table.insert(floating_wins, w)
-      end
-    end
-    if 1 == #wins - #floating_wins - #tree_wins then
-      -- Should quit, so we close all invalid windows.
-      for _, w in ipairs(tree_wins) do
-        vim.api.nvim_win_close(w, true)
-      end
-    end
-  end
+-- highlight the yank (duh)
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = vim.api.nvim_create_augroup("highlight_yank", {}),
+    desc = "Hightlight selection on yank",
+    pattern = "*",
+    callback = function()
+        vim.highlight.on_yank { higroup = "IncSearch", timeout = 500 }
+    end,
 })
