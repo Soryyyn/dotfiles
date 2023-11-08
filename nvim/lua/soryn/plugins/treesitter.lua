b0b1397c1e7893f12d3d67f4eb9286451f8aa265
Soryn Bächli <soryn.baechli@gmail.com>
2023-11-08 20:42:04 +0100

    fix: Hopefully fixing commentstring + tag closing in react

--- nvim/lua/soryn/plugins/treesitter.lua
+++ nvim/lua/soryn/plugins/treesitter.lua
@@ -1,43 +1,47 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        event = "VeryLazy",
-        build = ":TSUpdate",
-        dependencies = {
-            "nvim-treesitter/playground",
-            "JoosepAlviste/nvim-ts-context-commentstring"
-        },
-        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {"lua", "typescript", "javascript", "html", "json", "markdown"}, -- make sure the following parser are installed
-                sync_install = false,
-                auto_install = true,
-                indent = {
-                    enable = true
-                },
-                highlight = {
-                    enable = true,
-                },
-                incremental_selection = { -- increment selection of syntax
-                    enable = true,
-                    keymaps = {
-                        init_selection = "<C-Space>",
-                        node_incremental = "<C-Space>",
-                    }
-                },
-                autotag = {
-                    enable = true
-                }
-            })
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = "VeryLazy",
+		build = ":TSUpdate",
+		dependencies = {
+			"nvim-treesitter/playground",
+			"JoosepAlviste/nvim-ts-context-commentstring",
+		},
+		cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "lua", "typescript", "javascript", "html", "json", "markdown" }, -- make sure the following parser are installed
+				sync_install = false,
+				auto_install = true,
+				indent = {
+					enable = true,
+				},
+				highlight = {
+					enable = true,
+				},
+				incremental_selection = { -- increment selection of syntax
+					enable = true,
+					keymaps = {
+						init_selection = "<C-Space>",
+						node_incremental = "<C-Space>",
+					},
+				},
+				autotag = {
+					enable = true,
+					enable_close_on_slash = true,
+				},
+				context_commentstring = {
+					enable = true,
+				},
+			})
 
-            -- commenting in mixed frontend context
-            require("ts_context_commentstring").setup({})
-        end
-    },
-    {
-        "windwp/nvim-ts-autotag", -- auto edit html tags based on treesitter context
-        event = "InsertEnter",
-        opts = {}
-    }
+			-- commenting in mixed frontend context
+			require("ts_context_commentstring").setup({})
+		end,
+	},
+	{
+		"windwp/nvim-ts-autotag", -- auto edit html tags based on treesitter context
+		event = "InsertEnter",
+		opts = {},
+	},
 }
