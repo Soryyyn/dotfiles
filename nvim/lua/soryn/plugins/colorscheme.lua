67f1d05c097a4bfe3d967a971773da10d87caec9
Soryn Bächli <soryn.baechli@gmail.com>
2024-01-05 13:39:23 +0100

    chore: Some colorscheme changes

--- nvim/lua/soryn/plugins/colorscheme.lua
+++ nvim/lua/soryn/plugins/colorscheme.lua
@@ -1,34 +1,36 @@
 return {
-    "EdenEast/nightfox.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("nightfox").setup({
-            options = {
-                terminal_colors = true,
-                module_default = true,
-                hide_end_of_buffer = false,
-                inverse = { -- inverse colors
-                    match_paren = false,
-                    visual = true,
-                    search = false,
-                },
-                styles = { -- set font styling based on word
-                    comments = "italic",
-                },
-            },
-            groups = {
-                all = {
-                    illuminatedWordRead = {
-                        bg = "bg2"
-                    },
-                    illuminatedWordWrite = {
-                        bg = "bg2"
-                    },
-                }
-            }
-        })
+	"EdenEast/nightfox.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("nightfox").setup({
+			options = {
+				terminal_colors = true,
+				module_default = true,
+				hide_end_of_buffer = false,
+				inverse = { -- inverse colors
+					match_paren = false,
+					visual = true,
+					search = false,
+				},
+				styles = { -- set font styling based on word
+					comments = "italic",
+					keywords = "bold",
+					types = "italic,bold",
+				},
+			},
+			groups = {
+				all = {
+					illuminatedWordRead = {
+						bg = "bg2",
+					},
+					illuminatedWordWrite = {
+						bg = "bg2",
+					},
+				},
+			},
+		})
 
-        vim.cmd("colorscheme carbonfox")
-    end
+		vim.cmd("colorscheme carbonfox")
+	end,
 }
