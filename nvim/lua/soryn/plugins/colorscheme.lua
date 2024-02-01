4e28f32a715be436d4f8c5fdb48f7a796599fb31
Soryn Bächli <soryn.baechli@gmail.com>
2024-02-01 12:43:14 +0100

    feat: Add github colorscheme

--- nvim/lua/soryn/plugins/colorscheme.lua
+++ nvim/lua/soryn/plugins/colorscheme.lua
@@ -34,4 +34,37 @@ return {
 			vim.cmd("colorscheme carbonfox")
 		end,
 	},
+	{
+		"projekt0n/github-nvim-theme",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("github-theme").setup({
+				options = {
+					terminal_colors = true,
+					module_default = true,
+					inverse = { -- inverse colors
+						match_paren = false,
+						visual = true,
+						search = false,
+					},
+					styles = { -- set font styling based on word
+						comments = "italic",
+						keywords = "bold",
+						types = "italic,bold",
+					},
+				},
+				groups = {
+					all = {
+						illuminatedWordRead = {
+							bg = "bg2",
+						},
+						illuminatedWordWrite = {
+							bg = "bg2",
+						},
+					},
+				},
+			})
+		end,
+	},
 }
