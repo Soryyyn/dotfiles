4fdfaf32e517e93455c7b78f3fee00e3e7e2a052
Soryn Bächli <soryn.baechli@gmail.com>
2024-02-26 15:41:29 +0100

    feat: Using fork of colorizer to display colors

--- nvim/lua/soryn/plugins/editor.lua
+++ nvim/lua/soryn/plugins/editor.lua
@@ -1,150 +1,148 @@
 return {
-	{
-		"NvChad/nvim-colorizer.lua", -- color the background of colors
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			require("colorizer").setup()
-		end,
-	},
-	{
-		"themaxmarchuk/tailwindcss-colors.nvim", -- use tailwind lsp to highlight colors
-		name = "tailwindcss-colors",
-		event = "LspAttach",
-		config = function()
-			require("tailwindcss-colors").setup()
-		end,
-	},
-	{
-		"numToStr/Comment.nvim", -- comment out lines/blocks easier
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"JoosepAlviste/nvim-ts-context-commentstring",
-		},
-		config = function()
-			require("Comment").setup({
-				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-			})
-		end,
-	},
-	{
-		"kylechui/nvim-surround", -- surround things a looooot easier
-		version = "*",
-		event = "VeryLazy",
-		opts = {},
-	},
-	{
-		"windwp/nvim-autopairs", -- automatically add pairs for (){}[]''""``
-		event = "InsertEnter",
-		config = function()
-			require("nvim-autopairs").setup({
-				check_ts = true,
-			})
-		end,
-	},
-	{
-		"nvim-telescope/telescope.nvim", -- explorer
-		version = "*",
-		cmd = "Telescope",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "make",
-			},
-		},
-		config = function()
-			local telescope = require("telescope")
-			local actions = require("telescope.actions")
+    {
+        "JosefLitos/colorizer.nvim", -- color the background of colors (using fork currently for tailwind colors)
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("colorizer").setup({
+                user_default_options = {
+                    css = true,
+                    css_fn = true,
+                    names = 'tailwind_both',
+                }
+            })
+        end,
+    },
+    {
+        "numToStr/Comment.nvim", -- comment out lines/blocks easier
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "JoosepAlviste/nvim-ts-context-commentstring",
+        },
+        config = function()
+            require("Comment").setup({
+                pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+            })
+        end,
+    },
+    {
+        "kylechui/nvim-surround", -- surround things a looooot easier
+        version = "*",
+        event = "VeryLazy",
+        opts = {},
+    },
+    {
+        "windwp/nvim-autopairs", -- automatically add pairs for (){}[]''""``
+        event = "InsertEnter",
+        config = function()
+            require("nvim-autopairs").setup({
+                check_ts = true,
+            })
+        end,
+    },
+    {
+        "nvim-telescope/telescope.nvim", -- explorer
+        version = "*",
+        cmd = "Telescope",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
+            },
+        },
+        config = function()
+            local telescope = require("telescope")
+            local actions = require("telescope.actions")
 
-			telescope.setup({
-				defaults = {
-					color_devicons = true,
-					path_display = { "truncate" },
-					layout_strategy = "horizontal",
-					layout_config = {
-						horizontal = {
-							prompt_position = "top",
-						},
-					},
-					sorting_strategy = "ascending",
-					mappings = {
-						i = {
-							["<C-k>"] = actions.move_selection_previous,
-							["<C-j>"] = actions.move_selection_next,
-						},
-					},
-				},
-				pickers = {
-					buffers = {
-						show_all_buffers = true,
-						sort_mru = true,
-						mappings = {
-							n = {
-								["<DELETE>"] = actions.delete_buffer,
-							},
-						},
-					},
-				},
-			})
+            telescope.setup({
+                defaults = {
+                    color_devicons = true,
+                    path_display = { "truncate" },
+                    layout_strategy = "horizontal",
+                    layout_config = {
+                        horizontal = {
+                            prompt_position = "top",
+                        },
+                    },
+                    sorting_strategy = "ascending",
+                    mappings = {
+                        i = {
+                            ["<C-k>"] = actions.move_selection_previous,
+                            ["<C-j>"] = actions.move_selection_next,
+                        },
+                    },
+                },
+                pickers = {
+                    buffers = {
+                        show_all_buffers = true,
+                        sort_mru = true,
+                        mappings = {
+                            n = {
+                                ["<DELETE>"] = actions.delete_buffer,
+                            },
+                        },
+                    },
+                },
+            })
 
-			telescope.load_extension("fzf")
-		end,
-	},
-	{
-		"RRethy/vim-illuminate", -- highlight same word when cursor is over it
-		event = { "BufReadPre", "BufNewFile" },
-		opts = {
-			large_file_cutoff = 2000,
-			large_file_overrides = {
-				providers = { "treesitter", "lsp", "regex" },
-			},
-			under_cursor = false,
-		},
-		config = function(_, opts)
-			local illuminate = require("illuminate")
+            telescope.load_extension("fzf")
+        end,
+    },
+    {
+        "RRethy/vim-illuminate", -- highlight same word when cursor is over it
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {
+            large_file_cutoff = 2000,
+            large_file_overrides = {
+                providers = { "treesitter", "lsp", "regex" },
+            },
+            under_cursor = false,
+        },
+        config = function(_, opts)
+            local illuminate = require("illuminate")
 
-			-- keybindings
-			vim.keymap.set(
-				"n",
-				"<C-r>",
-				illuminate.goto_next_reference,
-				{ desc = "Goto next reference", noremap = true }
-			)
+            -- keybindings
+            vim.keymap.set(
+                "n",
+                "<C-r>",
+                illuminate.goto_next_reference,
+                { desc = "Goto next reference", noremap = true }
+            )
 
-			illuminate.configure(opts)
-		end,
-	},
-	{
-		"hinell/move.nvim", -- move lines & blocks of code
-		event = { "BufReadPre", "BufNewFile" },
-	},
-	{
-		"axelvc/template-string.nvim", -- automatic template string converting
-		event = "VeryLazy",
-		opts = {},
-	},
-	{
-		"folke/todo-comments.nvim", -- highlight todo comments
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		event = { "BufReadPre", "BufNewFile" },
-		opts = {},
-	},
-	{
-		"cappyzawa/trim.nvim", -- automatically trim trailing whitespace
-		event = { "BufReadPre", "BufNewFile" },
-		opts = {},
-	},
-	{
-		"jinh0/eyeliner.nvim", -- quickscope f/F jumping
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			require("eyeliner").setup({
-				highlight_on_key = true,
-				dim = false,
-			})
-		end,
-	},
+            illuminate.configure(opts)
+        end,
+    },
+    {
+        "hinell/move.nvim", -- move lines & blocks of code
+        event = { "BufReadPre", "BufNewFile" },
+    },
+    {
+        "axelvc/template-string.nvim", -- automatic template string converting
+        event = "VeryLazy",
+        opts = {},
+    },
+    {
+        "folke/todo-comments.nvim", -- highlight todo comments
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {},
+    },
+    {
+        "cappyzawa/trim.nvim", -- automatically trim trailing whitespace
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {},
+    },
+    {
+        "jinh0/eyeliner.nvim", -- quickscope f/F jumping
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("eyeliner").setup({
+                highlight_on_key = true,
+                dim = false,
+            })
+        end,
+    },
 }
--- nvim/lua/soryn/plugins/lsp.lua
+++ nvim/lua/soryn/plugins/lsp.lua
@@ -1,254 +1,216 @@
 return {
-	{
-		"neovim/nvim-lspconfig", -- language server configuration
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"williamboman/mason.nvim",
-			"ray-x/lsp_signature.nvim",
-			"pmizio/typescript-tools.nvim",
-		},
-		opts = {
-			diagnostics = {
-				underline = true,
-				update_in_insert = false,
-				virtual_text = {
-					spacing = 4,
-					source = "if_many",
-					prefix = "●",
-				},
-				severity_sort = true,
-			},
-			capabilities = require("cmp_nvim_lsp").default_capabilities(),
-		},
-		config = function(_, opts)
-			local lspconfig = require("lspconfig")
+    {
+        "neovim/nvim-lspconfig", -- language server configuration
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "williamboman/mason.nvim",
+            "ray-x/lsp_signature.nvim",
+            "pmizio/typescript-tools.nvim",
+        },
+        opts = {
+            diagnostics = {
+                underline = true,
+                update_in_insert = false,
+                virtual_text = {
+                    spacing = 4,
+                    source = "if_many",
+                    prefix = "●",
+                },
+                severity_sort = true,
+            },
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+        },
+        config = function(_, opts)
+            local lspconfig = require("lspconfig")
 
-			-- keybinds which are attached to the lsp
-			local on_attach = function(_, bufnr)
-				vim.keymap.set(
-					"n",
-					"gr",
-					"<CMD>Telescope lsp_references<CR>",
-					{ desc = "Show references", silent = true, buffer = bufnr }
-				)
-				vim.keymap.set(
-					"n",
-					"gd",
-					"<CMD>Telescope lsp_definitions<CR>",
-					{ desc = "Show definition(s)", silent = true, buffer = bufnr }
-				)
-				vim.keymap.set(
-					"n",
-					"gD",
-					vim.lsp.buf.declaration,
-					{ desc = "Go to declaration", silent = true, buffer = bufnr }
-				)
-				vim.keymap.set(
-					"n",
-					"gi",
-					"<CMD>Telescope lsp_implementations<CR>",
-					{ desc = "Show implementations", silent = true, buffer = bufnr }
-				)
-				vim.keymap.set(
-					"n",
-					"gt",
-					"<CMD>Telescope lsp_type_definitions<CR>",
-					{ desc = "Show type definitions", silent = true, buffer = bufnr }
-				)
-				vim.keymap.set(
-					{ "n", "v" },
-					"<leader>ca",
-					vim.lsp.buf.code_action,
-					{ desc = "See available code actions", silent = true, buffer = bufnr, noremap = true }
-				)
-				vim.keymap.set(
-					"n",
-					"K",
-					vim.lsp.buf.hover,
-					{ desc = "Hover information", remap = true, silent = true, buffer = bufnr }
-				)
-				vim.keymap.set(
-					"n",
-					"<leader>cf",
-					vim.lsp.buf.format,
-					{ desc = "Format code", silent = true, buffer = bufnr }
-				)
-				vim.keymap.set(
-					"n",
-					"<leader>cr",
-					vim.lsp.buf.rename,
-					{ desc = "Rename variable", silent = true, buffer = bufnr }
-				)
-				vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Goto previous diagnostic" })
-				vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Goto next diagnostic" })
+            -- keybinds which are attached to the lsp
+            local on_attach = function(_, bufnr)
+                vim.keymap.set(
+                    "n",
+                    "gr",
+                    "<CMD>Telescope lsp_references<CR>",
+                    { desc = "Show references", silent = true, buffer = bufnr }
+                )
+                vim.keymap.set(
+                    "n",
+                    "gd",
+                    "<CMD>Telescope lsp_definitions<CR>",
+                    { desc = "Show definition(s)", silent = true, buffer = bufnr }
+                )
+                vim.keymap.set(
+                    "n",
+                    "gD",
+                    vim.lsp.buf.declaration,
+                    { desc = "Go to declaration", silent = true, buffer = bufnr }
+                )
+                vim.keymap.set(
+                    "n",
+                    "gi",
+                    "<CMD>Telescope lsp_implementations<CR>",
+                    { desc = "Show implementations", silent = true, buffer = bufnr }
+                )
+                vim.keymap.set(
+                    "n",
+                    "gt",
+                    "<CMD>Telescope lsp_type_definitions<CR>",
+                    { desc = "Show type definitions", silent = true, buffer = bufnr }
+                )
+                vim.keymap.set(
+                    { "n", "v" },
+                    "<leader>ca",
+                    vim.lsp.buf.code_action,
+                    { desc = "See available code actions", silent = true, buffer = bufnr, noremap = true }
+                )
+                vim.keymap.set(
+                    "n",
+                    "K",
+                    vim.lsp.buf.hover,
+                    { desc = "Hover information", remap = true, silent = true, buffer = bufnr }
+                )
+                vim.keymap.set(
+                    "n",
+                    "<leader>cf",
+                    vim.lsp.buf.format,
+                    { desc = "Format code", silent = true, buffer = bufnr }
+                )
+                vim.keymap.set(
+                    "n",
+                    "<leader>cr",
+                    vim.lsp.buf.rename,
+                    { desc = "Rename variable", silent = true, buffer = bufnr }
+                )
+                vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Goto previous diagnostic" })
+                vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Goto next diagnostic" })
 
-				-- attach function signature help
-				require("lsp_signature").on_attach({
-					floating_window_above_cur_line = false,
-					floating_window = false,
-					hint_prefix = " ",
-				}, bufnr)
-			end
+                -- attach function signature help
+                require("lsp_signature").on_attach({
+                    floating_window_above_cur_line = false,
+                    floating_window = false,
+                    hint_prefix = " ",
+                }, bufnr)
+            end
 
-			-- change diagnostic icons in gutter
-			local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-			for type, icon in pairs(signs) do
-				local hl = "DiagnosticSign" .. type
-				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-			end
+            -- change diagnostic icons in gutter
+            local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+            for type, icon in pairs(signs) do
+                local hl = "DiagnosticSign" .. type
+                vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+            end
 
-			-- setup for each lsp server
-			require("mason-lspconfig").setup_handlers({
-				-- default server setup (will get overwritten if the server is defined below)
-				function(server_name)
-					lspconfig[server_name].setup({
-						capabilities = opts.capabilities,
-						on_attach = on_attach,
-					})
-				end,
+            -- setup for each lsp server
+            require("mason-lspconfig").setup_handlers({
+                -- default server setup (will get overwritten if the server is defined below)
+                function(server_name)
+                    lspconfig[server_name].setup({
+                        capabilities = opts.capabilities,
+                        on_attach = on_attach,
+                    })
+                end,
 
-				["lua_ls"] = function()
-					lspconfig["lua_ls"].setup({
-						capabilities = opts.capabilities,
-						on_attach = on_attach,
-						settings = {
-							Lua = {
-								diagnostics = {
-									globals = { "vim" },
-								},
-								workspace = {
-									library = {
-										[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-										[vim.fn.stdpath("config") .. "/lua"] = true,
-									},
-								},
-							},
-						},
-					})
-				end,
+                ["lua_ls"] = function()
+                    lspconfig["lua_ls"].setup({
+                        capabilities = opts.capabilities,
+                        on_attach = on_attach,
+                        settings = {
+                            Lua = {
+                                diagnostics = {
+                                    globals = { "vim" },
+                                },
+                                workspace = {
+                                    library = {
+                                        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                                        [vim.fn.stdpath("config") .. "/lua"] = true,
+                                    },
+                                },
+                            },
+                        },
+                    })
+                end,
 
-				["tailwindcss"] = function()
-					lspconfig["tailwindcss"].setup({
-						capabilities = opts.capabilities,
-						on_attach = function(_, bufnr)
-							require("tailwindcss-colors").buf_attach(bufnr)
-							on_attach(_, bufnr)
-						end,
-					})
-				end,
+                ["tailwindcss"] = function()
+                    lspconfig["tailwindcss"].setup({
+                        capabilities = opts.capabilities,
+                        on_attach = function(_, bufnr)
+                            require("tailwindcss-colors").buf_attach(bufnr)
+                            on_attach(_, bufnr)
+                        end,
+                    })
+                end,
 
-				["tsserver"] = function()
-					require("typescript-tools").setup({
-						on_attach = on_attach,
-						settings = {
-							expose_as_code_action = "all",
-						},
-					})
-				end,
-			})
-		end,
-	},
-	{
-		"williamboman/mason.nvim", -- language server installation, updating, etc
-		cmd = "Mason",
-		build = ":MasonUpdate",
-		dependencies = {
-			"williamboman/mason-lspconfig.nvim",
-		},
-		opts = {
-			ensure_installed = {
-				"stylua",
-				"shfmt",
-				"tsserver",
-				"eslint_d",
-				"prettier",
-				"emmet_ls",
-				"cssls",
-				"html",
-				"tailwindcss",
-			},
-			automatic_installation = true,
-		},
-		config = function(_, opts)
-			require("mason").setup(opts)
-		end,
-	},
-	-- {
-	-- 	"nvimtools/none-ls.nvim", -- formatting & linting
-	-- 	event = "LspAttach",
-	-- 	dependencies = {
-	-- 		"nvim-lua/plenary.nvim",
-	-- 	},
-	-- 	config = function()
-	-- 		local none_ls = require("null-ls")
-	-- 		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-	--
-	-- 		none_ls.setup({
-	-- 			sources = {
-	-- 				-- lua
-	-- 				none_ls.builtins.formatting.stylua,
-	--
-	-- 				-- eslint
-	-- 				none_ls.builtins.code_actions.eslint_d,
-	-- 				none_ls.builtins.diagnostics.eslint_d,
-	-- 				none_ls.builtins.formatting.eslint_d,
-	--
-	-- 				-- prettier
-	-- 				none_ls.builtins.formatting.prettier,
-	-- 			},
-	-- 			on_attach = function(client, bufnr) -- format the file on save
-	-- 				if client.supports_method("textDocument/formatting") then
-	-- 					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-	-- 					vim.api.nvim_create_autocmd("BufWritePre", {
-	-- 						group = augroup,
-	-- 						buffer = bufnr,
-	-- 						callback = function()
-	-- 							vim.lsp.buf.format({ async = false })
-	-- 						end,
-	-- 					})
-	-- 				end
-	-- 			end,
-	-- 		})
-	-- 	end,
-	-- },
-	{
-		"pmizio/typescript-tools.nvim", -- additional typescript lsp tooling
-		event = "LspAttach",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"neovim/nvim-lspconfig",
-		},
-		opts = {},
-	},
-	{
-		"zeioth/garbage-day.nvim", -- free RAM by stopping lsp when idled to long
-		dependencies = {
-			"neovim/nvim-lspconfig",
-		},
-		event = "VeryLazy",
-		opts = {
-			notifications = true,
-		},
-	},
-	{
-		"antosha417/nvim-lsp-file-operations", -- trigger lsp actions when moving/renaming files
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-neo-tree/neo-tree.nvim",
-		},
-		config = function()
-			require("lsp-file-operations").setup()
-		end,
-	},
-	{
-		"j-hui/fidget.nvim", -- lsp operations notifications
-		opts = {
-			progress = {
-				display = {
-					render_limit = 6,
-					done_ttl = 1,
-				},
-			},
-		},
-	},
+                ["tsserver"] = function()
+                    require("typescript-tools").setup({
+                        on_attach = on_attach,
+                        settings = {
+                            expose_as_code_action = "all",
+                        },
+                    })
+                end,
+            })
+        end,
+    },
+    {
+        "williamboman/mason.nvim", -- language server installation, updating, etc
+        cmd = "Mason",
+        build = ":MasonUpdate",
+        dependencies = {
+            "williamboman/mason-lspconfig.nvim",
+        },
+        opts = {
+            ensure_installed = {
+                "stylua",
+                "shfmt",
+                "tsserver",
+                "eslint_d",
+                "prettier",
+                "emmet_ls",
+                "cssls",
+                "html",
+                "tailwindcss",
+            },
+            automatic_installation = true,
+        },
+        config = function(_, opts)
+            require("mason").setup(opts)
+        end,
+    },
+    {
+        "pmizio/typescript-tools.nvim", -- additional typescript lsp tooling
+        event = "LspAttach",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "neovim/nvim-lspconfig",
+        },
+        opts = {},
+    },
+    {
+        "zeioth/garbage-day.nvim", -- free RAM by stopping lsp when idled to long
+        dependencies = {
+            "neovim/nvim-lspconfig",
+        },
+        event = "VeryLazy",
+        opts = {
+            notifications = true,
+        },
+    },
+    {
+        "antosha417/nvim-lsp-file-operations", -- trigger lsp actions when moving/renaming files
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-neo-tree/neo-tree.nvim",
+        },
+        config = function()
+            require("lsp-file-operations").setup()
+        end,
+    },
+    {
+        "j-hui/fidget.nvim", -- lsp operations notifications
+        opts = {
+            progress = {
+                display = {
+                    render_limit = 6,
+                    done_ttl = 1,
+                },
+            },
+        },
+    },
 }
