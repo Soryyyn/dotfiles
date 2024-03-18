ed926d03df6b589126e1dab42ae9a28e41e6c8f2
Soryn Bächli <soryn.baechli@gmail.com>
2024-03-18 23:51:25 +0100

    feat: Better rust setup

--- nvim/lua/soryn/plugins/treesitter.lua
+++ nvim/lua/soryn/plugins/treesitter.lua
@@ -1,44 +1,44 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		event = "VeryLazy",
-		build = ":TSUpdate",
-		dependencies = {
-			"nvim-treesitter/playground",
-			"nvim-treesitter/nvim-treesitter-textobjects",
-		},
-		cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "lua", "typescript", "javascript", "html", "json", "markdown" }, -- make sure the following parser are installed
-				sync_install = false,
-				auto_install = true,
-				indent = {
-					enable = true,
-				},
-				highlight = {
-					enable = true,
-				},
-				incremental_selection = { -- increment selection of syntax
-					enable = true,
-					keymaps = {
-						init_selection = "<C-Space>",
-						node_incremental = "<C-Space>",
-					},
-				},
-				textobjects = {
-					enable = true,
-				},
-				autotag = {
-					enable = true,
-					enable_close_on_slash = false,
-				},
-			})
-		end,
-	},
-	{
-		"windwp/nvim-ts-autotag", -- auto edit html tags based on treesitter context
-		event = "InsertEnter",
-		opts = {},
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = "VeryLazy",
+        build = ":TSUpdate",
+        dependencies = {
+            "nvim-treesitter/playground",
+            "nvim-treesitter/nvim-treesitter-textobjects",
+        },
+        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "lua", "typescript", "javascript", "html", "json", "markdown" }, -- make sure the following parser are installed
+                sync_install = false,
+                auto_install = true,
+                indent = {
+                    enable = true,
+                },
+                highlight = {
+                    enable = true,
+                },
+                incremental_selection = { -- increment selection of syntax
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<C-Space>",
+                        node_incremental = "<C-Space>",
+                    },
+                },
+                textobjects = {
+                    enable = true,
+                },
+                autotag = {
+                    enable = true,
+                    enable_close_on_slash = false,
+                },
+            })
+        end,
+    },
+    {
+        "windwp/nvim-ts-autotag", -- auto edit html tags based on treesitter context
+        event = "InsertEnter",
+        opts = {},
+    },
 }
