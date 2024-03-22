b42952e30c7bdbd035c0fd2c84cb25fa23dd6596
Soryn Bächli <soryn.baechli@gmail.com>
2024-03-23 00:30:22 +0100

    feat: Update lualine config

--- nvim/lua/soryn/plugins/ui.lua
+++ nvim/lua/soryn/plugins/ui.lua
@@ -47,6 +47,14 @@ return {
                         },
                     },
                 },
+                sections = {
+                    lualine_a = { 'mode' },
+                    lualine_b = { 'branch', 'diff', 'diagnostics' },
+                    lualine_c = { { 'filename', path = 1 } },
+                    lualine_x = { 'encoding', 'filetype' },
+                    lualine_y = { 'progress' },
+                    lualine_z = { 'location' }
+                },
             })
         end,
     },
