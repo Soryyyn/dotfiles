40d6a16447eae93ee70de1d2e5f6805d1d586269
Soryn Bächli <soryn.baechli@gmail.com>
2024-06-04 13:27:11 +0200

    feat: Update default theme to oldworld

--- nvim/lua/soryn/plugins/colorscheme.lua
+++ nvim/lua/soryn/plugins/colorscheme.lua
@@ -1,117 +1,117 @@
 return {
-    {
-        "EdenEast/nightfox.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require("nightfox").setup({
-                options = {
-                    terminal_colors = true,
-                    module_default = true,
-                    inverse = { -- inverse colors
-                        match_paren = false,
-                        visual = true,
-                        search = false,
-                    },
-                    styles = { -- set font styling based on word
-                        comments = "italic",
-                        keywords = "bold",
-                        types = "italic,bold",
-                    },
-                },
-                groups = {
-                    all = {
-                        illuminatedWordRead = {
-                            bg = "bg2",
-                        },
-                        illuminatedWordWrite = {
-                            bg = "bg2",
-                        },
-                    },
-                },
-            })
-        end,
-    },
-    {
-        "projekt0n/github-nvim-theme",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require("github-theme").setup({
-                options = {
-                    terminal_colors = true,
-                    module_default = true,
-                    styles = { -- set font styling based on word
-                        comments = "italic",
-                        keywords = "bold",
-                        types = "italic,bold",
-                    },
-                },
-                groups = {
-                    all = {
-                        illuminatedWordRead = {
-                            bg = "bg2",
-                        },
-                        illuminatedWordWrite = {
-                            bg = "bg2",
-                        },
-                    },
-                },
-            })
+	{
+		"EdenEast/nightfox.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("nightfox").setup({
+				options = {
+					terminal_colors = true,
+					module_default = true,
+					inverse = { -- inverse colors
+						match_paren = false,
+						visual = true,
+						search = false,
+					},
+					styles = { -- set font styling based on word
+						comments = "italic",
+						keywords = "bold",
+						types = "italic,bold",
+					},
+				},
+				groups = {
+					all = {
+						illuminatedWordRead = {
+							bg = "bg2",
+						},
+						illuminatedWordWrite = {
+							bg = "bg2",
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"projekt0n/github-nvim-theme",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("github-theme").setup({
+				options = {
+					terminal_colors = true,
+					module_default = true,
+					styles = { -- set font styling based on word
+						comments = "italic",
+						keywords = "bold",
+						types = "italic,bold",
+					},
+				},
+				groups = {
+					all = {
+						illuminatedWordRead = {
+							bg = "bg2",
+						},
+						illuminatedWordWrite = {
+							bg = "bg2",
+						},
+					},
+				},
+			})
 
-            -- vim.cmd("colorscheme github_dark_default")
-        end,
-    },
-    {
-        "Mofiqul/vscode.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require("vscode").setup({
-                italic_comments = true,
-            })
-        end,
-    },
-    {
-        "nyoom-engineering/oxocarbon.nvim",
-        lazy = false,
-        priority = 1000,
-    },
-    {
-        "catppuccin/nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup({
-                flavour = "mocha",
-                styles = {
-                    comments = { "italic" },
-                    keywords = { "bold" },
-                    types = { "italic", "bold" },
-                },
-            })
+			-- vim.cmd("colorscheme github_dark_default")
+		end,
+	},
+	{
+		"Mofiqul/vscode.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("vscode").setup({
+				italic_comments = true,
+			})
+		end,
+	},
+	{
+		"nyoom-engineering/oxocarbon.nvim",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+				styles = {
+					comments = { "italic" },
+					keywords = { "bold" },
+					types = { "italic", "bold" },
+				},
+			})
+		end,
+	},
+	{
+		"dgox16/oldworld.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("oldworld").setup({
+				styles = {
+					comments = {
+						italic = true,
+					},
+					keywords = {
+						bold = true,
+					},
+				},
+				integrations = {
+					neo_tree = true,
+				},
+			})
 
-            vim.cmd("colorscheme catppuccin-mocha")
-        end,
-    },
-    {
-        "dgox16/oldworld.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require("oldworld").setup({
-                styles = {
-                    comments = {
-                        italic = true
-                    },
-                    keywords = {
-                        bold = true
-                    },
-                },
-                integrations = {
-                    neo_tree = true
-                }
-            })
-        end
-    }
+			vim.cmd("colorscheme oldworld")
+		end,
+	},
 }
