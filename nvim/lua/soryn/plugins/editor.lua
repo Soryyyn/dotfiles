4199349da09c5e72625ac832021d218678442fc7
Soryn Bächli <soryn.baechli@gmail.com>
2025-02-25 12:01:49 +0100

    feat: Add clasp

--- nvim/lua/soryn/plugins/editor.lua
+++ nvim/lua/soryn/plugins/editor.lua
@@ -134,5 +134,20 @@ return {
                 highlight_on_key = true
             })
         end
+    },
+    {
+        "xzbdmw/clasp.nvim",
+        config = function()
+            require("clasp").setup({
+                pairs = { ["{"] = "}", ['"'] = '"', ["'"] = "'", ["("] = ")", ["["] = "]", ["<"] = ">" },
+                -- If called from insert mode, do not return to normal mode.
+                keep_insert_mode = true,
+            })
+
+            -- jumping from smallest region to largest region
+            vim.keymap.set({ "n", "i" }, "<c-l>", function()
+                require("clasp").wrap('next')
+            end)
+        end
     }
 }
