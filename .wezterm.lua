78988a81df6b4a6b978bcf0642937715543e3659
Soryn Bächli <soryn.baechli@gmail.com>
2025-03-20 22:31:34 +0100

    feat: Fix windows config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,16 +1,40 @@
 local wezterm = require('wezterm')
 
+-- detect os
+local is_darwin <const> = wezterm.target_triple:find("darwin") ~= nil
+local is_windows <const> = wezterm.target_triple:find("windows") ~= nil
+
 local config = wezterm.config_builder()
 
-config.font = wezterm.font('JetBrainsMono Nerd Font Mono')
-config.font_size = 14
+-- macos specific config
+if is_darwin then
+    config.font_size = 14
 
--- Disable bar
+    config.window_background_opacity = 0.8
+end
+
+-- windows specific config
+if is_windows then
+    config.wsl_domains = {
+        {
+            name = 'WSL:Ubuntu',
+            distribution = 'Ubuntu',
+        },
+    }
+    config.default_domain = 'WSL:Ubuntu'
+
+    config.font_size = 12
+
+    config.window_background_opacity = 0.9
+end
+
+config.font = wezterm.font('JetBrainsMono Nerd Font Mono')
+
+-- disable bar
 config.enable_tab_bar = false
 config.window_decorations = 'RESIZE'
 
 -- background opacity
-config.window_background_opacity = 0.8
 config.macos_window_background_blur = 40
 
 -- colorscheme
