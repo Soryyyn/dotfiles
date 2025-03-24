a97f6a4dcb67ed6aa135aede328279c0e1742da2
Soryn Bächli <soryn.baechli@gmail.com>
2025-03-24 10:29:21 +0100

    feat: Add additional telescope args to include more files

--- .config/nvim/lua/soryn/plugins/editor.lua
+++ .config/nvim/lua/soryn/plugins/editor.lua
@@ -52,6 +52,27 @@ return {
 
             telescope.setup({
                 defaults = {
+                    vimgrep_arguments = {
+                        "rg",
+                        "--follow",        -- Follow symbolic links
+                        "--hidden",        -- Search for hidden files
+                        "--no-heading",    -- Don't group matches by each file
+                        "--with-filename", -- Print the file path with the matched lines
+                        "--line-number",   -- Show line numbers
+                        "--column",        -- Show column numbers
+                        "--smart-case",    -- Smart case search
+
+                        -- Exclude some patterns from search
+                        "--glob=!**/.git/*",
+                        "--glob=!**/.idea/*",
+                        "--glob=!**/.vscode/*",
+                        "--glob=!**/build/*",
+                        "--glob=!**/dist/*",
+                        "--glob=!**/yarn.lock",
+                        "--glob=!**/package-lock.json",
+                        "--glob=!**/node_modules/*",
+                        "--glob=!**/.next/*",
+                    },
                     color_devicons = true,
                     path_display = { "truncate" },
                     layout_strategy = "horizontal",
@@ -79,6 +100,25 @@ return {
                             n = {
                                 ["<DELETE>"] = actions.delete_buffer,
                             },
+                        },
+                    },
+                    find_files = {
+                        hidden = true,
+                        -- needed to exclude some files & dirs from general search
+                        -- when not included or specified in .gitignore
+                        find_command = {
+                            "rg",
+                            "--files",
+                            "--hidden",
+                            "--glob=!**/.git/*",
+                            "--glob=!**/.idea/*",
+                            "--glob=!**/.vscode/*",
+                            "--glob=!**/build/*",
+                            "--glob=!**/dist/*",
+                            "--glob=!**/yarn.lock",
+                            "--glob=!**/package-lock.json",
+                            "--glob=!**/node_modules/*",
+                            "--glob=!**/.next/*",
                         },
                     },
                 },
